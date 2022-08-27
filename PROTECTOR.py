d8303891b44764841ccc9489884fcb47a8771e0c
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2022-08-27 18:10:02 +0500

    Update PROTECTOR.py

--- PROTECTOR.py
+++ PROTECTOR.py
@@ -1,62 +1,14 @@
+
 import os, platform
-
- 
-
 try:
-
- 
-
         import requests
-
- 
-
 except:
-
- 
-
         os.system('pip2 install requests')
-
- 
-
- 
-
- 
-
 import requests
-
- 
-
 bit = platform.architecture()[0]
-
- 
-
 if bit == "64bit":
-
- 
-
-        from Hamii import main
-
- 
-
-        main()
-
- 
-
- 
-
- 
-
+        from PROTECT64 import menu
+        menu()
 elif bit == "32bit":
-
- 
-
-        from King import main
-      
-
- 
-
- 
-
-        S()
-
- 
+        from PROTECT32 import menu
+        menu()
